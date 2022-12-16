c9de9c294324bc0a197f0d07e4288cd3a4f44454
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2022-12-16 13:58:22 -0500

    Pass all cypress test

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+/* Changes color of current*/
+const current = document.querySelector("#current-color")
+const pallet = document.querySelectorAll(".color");
+
+pallet.forEach(color => color.addEventListener("click", () => {
+  current.style.backgroundColor = color.style.backgroundColor
+})
+)
+
+/* Change cell color */
+const cells = document.querySelectorAll(".cell")
+
+cells.forEach(cell => cell.addEventListener("click", () => {
+  cell.style.backgroundColor = current.style.backgroundColor 
+})
+)
